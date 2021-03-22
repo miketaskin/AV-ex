c3161b885b6b795517e2ca2441f5318867b699b8
miketaskin <mih.taskin@yandex.ru>
2021-03-22 21:26:11 +0300

    create_rotate_image_function

---
+++ image_rotation.py
@@ -0,0 +1,29 @@
+import os
+import pathlib
+import sys
+from PIL import Image
+
+
+def rotate_image_on_180(image):
+    # image.width-ширина
+    # image.height-высота
+    # image.getpixel((x,y))
+    # image.putpixel((x,y),pixelvalue)-устанавливает
+    result = Image.new(image.mode, image.size)
+    for x in range(image.width):
+        for y in range(image.height):
+            result.putpixel((x, y), image.getpixel((image.width - 1 - x, image.height - 1 - y)))
+    return result
+
+
+def main(image_path_str):
+    working_dir_path = ''
+    if not os.path.isabs(image_path_str):
+        working_dir_path = pathlib.Path().absolute()
+    full_image_path = os.path.join(working_dir_path, image_path_str)
+    image = Image.open(full_image_path)
+    rotate_image_on_180(image).show()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
