ee201430a8d99807cdb202a88d271cef7a10158d
miketaskin <mih.taskin@yandex.ru>
2021-04-05 23:06:36 +0300

    resampling

--- image_resampling.py
+++ image_resampling.py
@@ -66,6 +66,24 @@ def image_upsampling_interpolation(image, n):
     return result
 
 
+def image_resampling_two_pass(image, n, m):
+    result = image_upsampling_interpolation(image, n)
+    return image_downsampling_decimation(result, m)
+
+
+def image_resamoling_one_pass(image, n, m):
+    if not isint(n) or n <= 0 or not isint(m) or m <= 0:
+        raise ValueError
+
+    result = Image.new(image.mode, (int(round(image.width * n / m) - 1), int(round(image.height * n / m) - 1)))
+
+    for x in range(result.width):
+        for y in range(result.height):
+            result.putpixel((x, y), image.getpixel((int(round(x * m / n)), int(round(y * m / n)))))
+
+    return result
+
+
 def rgb_lerp(color1, color2, t):
     r = int(round((color2[0] - color1[0]) * t + color1[0]))
     g = int(round((color2[1] - color1[1]) * t + color1[1]))
@@ -80,9 +98,11 @@ def main(image_path_str):
         working_dir_path = pathlib.Path().absolute()
     full_image_path = os.path.join(working_dir_path, image_path_str)
     image = Image.open(full_image_path)
-    image_upsampling_closest_neighbour(image, 3).save("result.png")
+    #image_upsampling_closest_neighbour(image, 3).save("result.png")
     # image_downsampling_decimation(image, 3).save("result1.png")
-    image_upsampling_interpolation(image, 3).save("result1.png")
+    #image_upsampling_interpolation(image, 3).save("result1.png")
+    image_resampling_two_pass(image, 3, 2).save("result1.png")
+    image_resamoling_one_pass(image, 3, 2).save("result2.png")
 
 
 if __name__ == '__main__':
